87987235c0cc9ce9ea3e01e7e17675161441c88a
achifred <achifred123@gmail.com>
2019-10-25 02:27:26 +0000

    deleted server.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Chat from './pages/chat';
 import Join from './pages/join';
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact render={props => <Join {...props} />} />
-        <Route path="/chat" exact render={props => <Chat {...props} />} />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" exact render={props => <Join {...props} />} />
+            <Route path="/chat" exact render={props => <Chat {...props} />} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
